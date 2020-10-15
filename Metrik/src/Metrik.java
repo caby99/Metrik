074d1a474644b934c7c4f5ec2f7bf5963f627ad3
Rene <Rene@192.168.178.108>
2020-10-15 17:19:00 +0200

    c

--- Metrik/src/Metrik.java
+++ Metrik/src/Metrik.java
@@ -3,7 +3,7 @@ public class Metrik {
 	public static void main(String[] args) {
 		int windowSizeX = 500;
 		int windowSizeY = 500;
-		
+		//so ein scheiss
 		MainWindow mw = new MainWindow(windowSizeX, windowSizeY);
 		mw.openWindow();
 	}
