ccdd7979edfbd52c28f08b523ce3e92e10b45c44
Kuma121 <darth.pascal1@web.de>
2020-10-18 20:13:18 +0200

    neu2

--- Metrik/src/DrawObject.java
+++ Metrik/src/DrawObject.java
@@ -9,6 +9,9 @@ public class DrawObject extends Canvas implements MouseListener{
 	private int _counter;
 	private int _sizeX = 0;
 	private int _sizeY = 0;
+	private Canvas canvas;
+	private String text = "Koordinaten";
+//	private int _mouseCounter = 0;
 	
 	public DrawObject(int sizeX, int sizeY, int color) {
 		this._sizeX = sizeX;
@@ -35,6 +38,7 @@ public class DrawObject extends Canvas implements MouseListener{
 
 	@Override
 	public void paint(Graphics g) {
+		
 		if(_x == 0 && _y == 0) return;
 		
 		Graphics2D g2d = (Graphics2D) g;
@@ -43,19 +47,39 @@ public class DrawObject extends Canvas implements MouseListener{
 		Graphics2D rec = (Graphics2D) g;
 		rec.setStroke(new BasicStroke(3.0F));
 		
+		Graphics2D line = (Graphics2D) g;
+		line.setStroke(new BasicStroke(3.0F));
+		
 		Ellipse2D ellipse2D = new Ellipse2D.Float(_x, _y, 50, 50);
 		Rectangle2D rectangle2D = new Rectangle2D.Float(_x, _y, 50, 50);
 		
-		//g2d.draw(ellipse2D);
-		rec.draw(rectangle2D);
-
-//		if(_counter > 1) {
-//			g2d.clearRect(0, 0, _sizeX, _sizeY);
-//			_counter = -1;
+		
+		g2d.draw(ellipse2D);
+		
+		if(_y < 20) {
+			g2d.drawString(text, _x-20, _y+65);
+		} else {
+			g2d.drawString(text, _x-20, _y-10);
+		}
+		
+//		Point2D.Float point1 = null;
+//		Point2D.Float point2 = null;
+//		
+//		if(_mouseCounter == 1) {
+//			point1 = new Point2D.Float(_x, _y);
+//		} else if(_mouseCounter == 2){
+//			point2 = new Point2D.Float(_x, _y);
+//		} else if(_mouseCounter > 2){
+//			Line2D line2D = new Line2D.Float(point1, point2);
+//			line.draw(line2D);
+//			_mouseCounter = 0;
 //		}
+		
+		
 		
 		if(_counter > 1) {
 			rec.clearRect(0, 0, _sizeX, _sizeY);
+			
 			_counter = -1;
 		}
 		
@@ -65,12 +89,16 @@ public class DrawObject extends Canvas implements MouseListener{
 	public void mouseClicked(MouseEvent e) {
 		_x = (float) e.getX();
 		_y = (float) e.getY();
-
+		text = "Koordinaten: x: "+_x+" y: "+_y;
+		
+		Graphics graphic = getGraphics();
 		paint(getGraphics());
 		_counter++;
+//		_mouseCounter++;
 		
 	}
-
+	
+	
 	@Override
 	public void mousePressed(MouseEvent e) {
 		
