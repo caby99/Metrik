d46b5f3a5067de629d4506d6111dda1ed92dd76e
Rene <Rene@MSI>
2020-10-20 13:34:06 +0200

    Entfernungsberechnung eingefügt

--- Metrik/src/DrawObject.java
+++ Metrik/src/DrawObject.java
@@ -12,6 +12,11 @@ public class DrawObject extends Canvas implements MouseListener{
 	private Canvas canvas;
 	private String text = "Koordinaten";
 //	private int _mouseCounter = 0;
+	private double _coordx1 =-1;
+	private double _coordy1 =-1;
+	private double _coordx2 =-1;
+	private double _coordy2 =-1;
+	private String entfernung = "Entfernung:";
 	
 	public DrawObject(int sizeX, int sizeY, int color) {
 		this._sizeX = sizeX;
@@ -53,8 +58,26 @@ public class DrawObject extends Canvas implements MouseListener{
 		Ellipse2D ellipse2D = new Ellipse2D.Float(_x, _y, 50, 50);
 		Rectangle2D rectangle2D = new Rectangle2D.Float(_x, _y, 50, 50);
 		
-		
+//		g2d.draw(rectangle2D);
 		g2d.draw(ellipse2D);
+		
+		
+		if (_counter == 0) {
+			_coordx1 = _x;
+			_coordy1 = _y;
+		}
+		else if (_counter == 1) {
+			_coordx2 = _x;
+			_coordy2 = _y;
+			entfernung = "Entfernung: "+calculateDistanceBetweenPointsWithPoint2D(_coordx1, _coordy1, _coordx2, _coordy2);
+			g2d.drawString(entfernung, 0, 15);
+			_coordx1 = _coordy1 = _coordx2 = _coordy2 = -1;
+		}
+//		else if (_counter == 2) {
+//			System.out.println( calculateDistanceBetweenPointsWithPoint2D(_coordx1, _coordy1, _coordx2, _coordy2)  );
+//			
+//		}
+		System.out.println(_x + ";"+_y +";"+ _counter +";" + _coordx1 +";" + _coordy1 +";"+ _coordx2 +";" +_coordy2 );
 		
 		if(_y < 20) {
 			g2d.drawString(text, _x-20, _y+65);
@@ -96,6 +119,11 @@ public class DrawObject extends Canvas implements MouseListener{
 		_counter++;
 //		_mouseCounter++;
 		
+	}
+	
+	public double calculateDistanceBetweenPointsWithPoint2D(double x1, double y1, double x2, double y2) {
+
+	    return Point2D.distance(x1, y1, x2, y2);
 	}
 	
 	
